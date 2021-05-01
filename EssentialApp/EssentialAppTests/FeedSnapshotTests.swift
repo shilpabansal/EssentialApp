12799841a34752e2d7e2b7782ee39e243e048d8a
Shilpa Bansal <shilpa.bansal@walmartlabs.com>
2021-05-01 16:44:13 +0530

    Record with feed content snapshot

--- EssentialApp/EssentialAppTests/FeedSnapshotTests.swift
+++ EssentialApp/EssentialAppTests/FeedSnapshotTests.swift
@@ -11,9 +11,16 @@ class FeedSnapshotTests: XCTestCase {
     func test_emptyFeeds() {
         let sut = makeSUT()
         sut.display(cellController: emptyFeeds())
-        let snapshot = sut.snapshot()
         
-        record(snapshot: snapshot, fileName: "EMPTY_Feed")
+        record(snapshot: sut.snapshot(), fileName: "EMPTY_Feed")
+    }
+    
+    func test_emptyFeedWithContent() {
+        let sut =  makeSUT ()
+
+        sut.display(feedWithContent())
+
+        record(snapshot: sut.snapshot(), fileName: "FEED_WITH_CONTENT_light")
     }
     
     //MARK: - Helpers
@@ -24,6 +31,21 @@ class FeedSnapshotTests: XCTestCase {
         let feedViewController = storyBoard.instantiateViewController(identifier: "FeedViewController") as! FeedViewController
         feedViewController.loadViewIfNeeded()
         return feedViewController
+    }
+    
+    private func feedWithContent() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            )
+        ]
     }
     
     private func emptyFeeds() -> [FeedImageCellController] {
@@ -52,11 +74,45 @@ class FeedSnapshotTests: XCTestCase {
     }
 }
 
+private extension FeedViewController {
+    func display(_ stubs: [ImageStub]) {
+        let cells: [FeedImageCellController] = stubs.map {
+            let cellController = FeedImageCellController(delegate: $0)
+            $0.cellController = cellController
+            return cellController
+        }
+        display(cellController: cells)
+    }
+}
+
 extension UIViewController {
     func snapshot() -> UIImage {
         let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
         return renderer.image { (action) in
             view.layer.render(in: action.cgContext)
         }
+    }
+}
+
+private class ImageStub: FeedImageCellControllerDelegate {
+    weak var cellController: FeedImageCellController?
+    var viewModel: FeedImageViewModel<UIImage>
+
+    init(description: String,
+         location: String,
+         image: UIImage) {
+        viewModel = FeedImageViewModel<UIImage>(location: location,
+                                                description: description,
+                                                image: image,
+                                                isLoading: false,
+                                                showRetry: false)
+    }
+    
+    func didRequestImage() {
+        cellController?.display(viewModel)
+    }
+    
+    func didCancelImageRequest() {
+        
     }
 }
